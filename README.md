30b8f9015cb022350fb7ca289fb5dd2b7ba44599
Tal Franji <tal.franji@gmail.com>
2016-12-22 15:47:51 +0200

    better doc

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # miscutil
 Useful scripts
 
-# classfind
+## classfind
 Help find Java class in a list of jars or paths
-Finds conflicts - same class in several different jars.
-Example of usage:
-$ classfind.py /usr/java/lib
 
-For example on a Spark/Hadoop cluster this is useful for finding if and where a class is defined:
-$ classfind.py -s s3file `hadoop class`
+    Usage classfind.py  [-s class-name-sub-string]  jar_or_dir_1 jar_or_dir_2...
+    Examples:
+        # prints this help:
+        $ classfind.py --help
+        # use -s . to find ALL class and print their names
+        $ classfind.py -s . /usr/local/hadoop/share/hadoop/common/lib/
+        # find conflicts between jars in two directories
+        $ classfind.py /usr/local/hadoop/share/hadoop/common/lib/ /usr/local/hadoop/share/hadoop/yarn/lib/
+        # find conflicts in hadoop class path using the shell special `command` notation
+        $ classfind.py  `hadoop classpath`
+        # find specific class - note - case insensitive
+        $ classfind.py  -s s3file `hadoop classpath` $SPARK_HOME/lib
+
+#Output
+
+$ classfind.py /usr/local/hadoop/share/hadoop/common/lib/ /usr/local/hadoop/share/hadoop/yarn/lib/
+
+duplicate: class org.apache.commons.beanutils.converters.ShortConverter appears in files:
+	/usr/local/hadoop/share/hadoop/common/lib/commons-beanutils-1.7.0.jar
+	/usr/local/hadoop/share/hadoop/common/lib/commons-beanutils-core-1.8.0.jar
+CONFLICT: class org.apache.commons.beanutils.converters.ShortConverter different versions in path commons-beanutils-1.7.0.jar;commons-beanutils-core-1.8.0.jar
+duplicate: class org.apache.commons.beanutils.locale.LocaleConvertUtils appears in files:
+	/usr/local/hadoop/share/hadoop/common/lib/commons-beanutils-1.7.0.jar
+	/usr/local/hadoop/share/hadoop/common/lib/commons-beanutils-core-1.8.0.jar
+CONFLICT: class org.apache.commons.beanutils.locale.LocaleConvertUtils different versions in path commons-beanutils-1.7.0.jar;commons-beanutils-core-1.8.0.jar
+duplicate: class org.apache.commons.beanutils.ConvertUtils appears in files:
+	/usr/local/hadoop/share/hadoop/common/lib/commons-beanutils-1.7.0.jar
+
+
+
--- classfind.py
+++ classfind.py
@@ -11,22 +11,36 @@ import sys
 
 
 
-def OSys(cmd):
-  #print 'Executing: ', cmd
-  args = shlex.split(cmd)
-  p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-  out, err = p.communicate()
-  if err:
-        print "ERROR =============\n", err
-  return out
+def OSys(cmd, verbose):
+    if verbose:
+        print 'Executing: ', cmd
+    args = shlex.split(cmd)
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    out, err = p.communicate()
+    if err:
+        if verbose:
+            print "ERROR =============\n", err
+        return None
+    return out
 
 
 def usage(message):
     print """%s
     Usage classfind.py  [-s class-name-sub-string]  jar_or_dir_1 jar_or_dir_2...
     Examples:
-        $ classfind.py --help  # prints this
-        $ 
+        # prints this help:
+        $ classfind.py --help
+        # use -s . to find ALL class and print their names
+        $ classfind.py -s . /usr/local/hadoop/share/hadoop/common/lib/
+        # find conflicts between jars in two directories
+        $ classfind.py /usr/local/hadoop/share/hadoop/common/lib/ /usr/local/hadoop/share/hadoop/yarn/lib/
+        # find conflicts in hadoop class path using the shell special `command` notation
+        $ classfind.py  `hadoop classpath`
+        # find specific class - note - case insensitive
+        $ classfind.py  -s s3file `hadoop classpath` $SPARK_HOME/lib
+
+
+
         You can also use `hadoop classpah` as parameter - it is split by ':'""" % message
 
 # Accept paths arg with the following opeion examples:
@@ -49,8 +63,9 @@ def FileIter(arg, pattern):
         if os.path.exists(path):
             if os.path.isdir(path):
                 for file in os.listdir(path):
-                    if yield_pred(file):
-                        yield file
+                    full = os.path.join(path, file)
+                    if yield_pred(full):
+                        yield full
             else:
                 if yield_pred(path):
                     yield path
@@ -63,7 +78,9 @@ def FileIter(arg, pattern):
 
 def IterClasses(jarfile):
     # using unzip and not jar since JDK is not installed
-    text = OSys("unzip -l " + jarfile)
+    text = OSys("unzip -l " + jarfile, False)
+    if not text:
+        return
     records = map(lambda s: re.split(r"\s+", s), text.splitlines())
     records = filter(lambda r: len(r) == 5 and r[4].find(".class") > 0, records)
     files = map(lambda r: r[4], records)
@@ -81,7 +98,7 @@ def main(argv):
         if len(argv) < 4:
             usage("missing sub string expression")
             return
-        find_substring = argv[2]
+        find_substring = argv[2].lower()
         ai = 3
     # class index - map between class name and list of files containing it
     class_index = {}
@@ -94,15 +111,20 @@ def main(argv):
                     print class_name
                 class_index.setdefault(class_name, []).append(file)
     # print conflicts:
-    print "CLASS conflicts:"
     for name, files in class_index.iteritems():
         if find_substring:
             if name.lower().find(find_substring) >= 0:
-                print "FOUND: class {} appears in files:\n\t{}".format(name, "\n\t".join(files))
+                print "found: class {} appears in files:\n\t{}".format(name, "\n\t".join(files))
         else:
             # if not defined find string - print all conflicts
             if len(files) > 1:
-                print "CONFLICT: class {} appears in files:\n\t{}".format(name, "\n\t".join(files))
+                versions = set()
+                for f in files:
+                    nopath = os.path.basename(f)
+                    versions.add(nopath)
+                print "duplicate: class {} appears in files:\n\t{}".format(name, "\n\t".join(files))
+                if len(versions) > 1:
+                    print "CONFLICT: class {} different versions in path {}".format(name, ";".join(versions))
 
 
 
