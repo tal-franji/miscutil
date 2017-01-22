0ed4adf3a50e769a91c7fb795d8d2bb2549008e1
Tal Franji <tal.franji@gmail.com>
2017-01-22 18:10:03 +0200

    Aws/EC2 CLI script functions

---
+++ awsutil.py
@@ -0,0 +1,279 @@
+#!/usr/bin/python
+# Simple wrapper around AWS CLI interface to create EC2 instances etc.
+# NO LICENCE , NO LIABILITIES
+# this is FREE and you are allowed to copy, change, slice  it - do whatever you want
+# this was written to Amazon Linux/CentOS
+__author__ = "tal.franji@gmail.com"
+
+
+import json
+import os
+import re
+import shlex
+import subprocess
+import sys
+
+
+# global defaults for AWS commands
+GlobalDefaults = {
+  'region': 'eu-west-1',
+   'ec2-username': 'ec2-user', # 'hadoop' for emr
+   'ssh-tunnel-ports': [4040, 8080, 8020, 9090, 8890, 9026],
+}
+
+# used by this module to read the defaults
+def getDefault(param):
+    global GlobalDefaults
+    return GlobalDefaults.get(param)
+
+#
+def setDefault(param, value):
+    global GlobalDefaults
+    GlobalDefaults[param] = value
+
+
+def System(cmd):
+  print 'Executing: ', cmd
+  return os.system(cmd)
+
+
+def OSys(cmd):
+  print 'Executing: ', cmd
+  args = shlex.split(cmd)
+  p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+  out, err = p.communicate()
+  if err:
+        print "ERROR =============\n", err
+  return out
+
+# assume cmd_template conatains %(xxx)s in it and use python % to fit in the params dict()
+def PSystem(cmd_template, params):
+    cmd = re.sub(r"\s+", " ", cmd_template)
+    cmd = cmd % params
+    return OSys(cmd)
+
+# add --key=value to the command based on params dict
+# return parsed json result
+def AwsSystem(cmd_head, params, useDefaultForNone):
+    # ignore params where value is None
+    effective_params = dict()
+    for k,v in params.iteritems():
+        if v is None:
+            if useDefaultForNone:
+                v = getDefault(k)
+        if v is not None:
+            effective_params[k] = v
+
+    cmd = cmd_head + " " + " ".join(["--%s %s" % (k, v) for k,v in  effective_params.iteritems()])
+    json_str = OSys(cmd)
+    if not json_str:
+        return None
+    try:
+        j = json.loads(json_str)
+        return j
+    except:
+        print "ERROR - Bad json from cmd=", cmd
+        print "response: ", json_str
+    return None
+
+# get EC2 instance id using meta-data
+def SelfInstnanceId():
+    if sys.platform == "win32" or sys.platform == "darwin":
+        return None  # when running script on laptop
+    return OSys("wget -q -O - http://instance-data/latest/meta-data/instance-id")
+
+# get Public IP for the instance
+def SelfInstancePubIp():
+    instance_id = SelfInstnanceId()
+    if not instance_id:
+        return "127.0.0.1"
+    return GetPublicIp(instance_id)
+
+# find instance by tag
+def InstanceOfTagValue(tag, value):
+    for inst in IterInstances():
+        if inst.tags_dict.get(tag) == value:
+            return inst
+    return None
+
+
+def CreateInstanceIfNotExist(singelton_tag_name=None,singelton_tag_value=None,
+                             iam_instance_profile=None, snapshot_volume = None, ami = None,
+                           tags={}, key_pair=None, security_group = None,
+                          instance_type=None):
+    if singelton_tag_name:
+        instance = InstanceOfTagValue(singelton_tag_name, singelton_tag_value)
+        if instance:
+            print "Instance already exists! for tag={} value={} instance={} ip={}".format(
+                singelton_tag_name, singelton_tag_value, instance.id, instance.pub_ip)
+            return
+    if 'Name' not in tags:
+        tags['Name'] = singelton_tag_value
+    tags[singelton_tag_name] = singelton_tag_value
+    CreateInstance(iam_instance_profile=iam_instance_profile, snapshot_volume=snapshot_volume, ami=ami,
+                          tags=tags, key_pair=key_pair, security_group=security_group,
+                          instance_type=instance_type)
+
+
+def CreateInstance(iam_instance_profile=None, snapshot_volume=None, ami=None,
+                          tags={}, key_pair=None, security_group=None,
+                          instance_type=None):
+    "--block-device-mappings file://mapping.json"
+    block_device_mapping = None
+    if snapshot_volume:
+        tmpjson = "/tmp/tmp.json"
+        with open(tmpjson, "w+t") as f:
+            f.write("""[
+                  {
+                    "DeviceName": "/dev/sdh",
+                    "Ebs": {
+                    "SnapshotId": "%s",
+                    "DeleteOnTermination": true
+                    }
+                  }
+                ]
+            """ % snapshot_volume)
+        block_device_mapping = "file://" + tmpjson
+    if not key_pair:
+        print "ERROR - missing key-pair"
+        return
+
+    availability_zone = None
+    if getDefault('region'):
+        availability_zone = getDefault('region') + 'a'
+    params = {'ami': ami,
+              'instance-type': instance_type,
+              'key-pair': key_pair,
+              'AvailabilityZone': availability_zone,
+              'region': None,
+              'iam-instance-profile': iam_instance_profile,
+              'block-device-mappings': block_device_mapping,
+              'security_group': security_group,
+              }
+    j = AwsSystem("aws ec2 run-instances", params, True)
+    instance_id = j.get("Instances", [{}])[0].get("InstanceId", None)
+    if not instance_id:
+        print "ERROR - bad response", str(j)
+        return
+    # looping over tags
+    # TODO(franji): there shoudlbe a way to add multiple tags in single command
+    # it does not work as documented - should try json parameter
+    cmd = 'aws ec2 create-tags'
+    params = {'resources': instance_id,'region': None}
+    for k, v in tags.iteritems():
+        params['tags'] = '"Key=%s,Value=%s"' % (k,v)
+        AwsSystem(cmd, params, True)
+
+    #tags_flag =  " ".join(['--tags "Key=%s,Value=%s"' % (k,v) for k, v in tags.iteritems()])
+    #cmd = 'aws ec2 create-tags ' + tags_flag
+    #AwsSystem(cmd, {'resources': instance_id,'region': region})
+    return instance_id
+
+
+# Hold EC2 instance attributes
+class InstanceInfo(object):
+    def __init__(self, inst_json):
+        self.json = inst_json
+        inst = inst_json
+        self.id = inst["InstanceId"]
+        self.pub_ip = inst.get("PublicIpAddress", "noIP")
+        tags_array = inst.get("Tags", [])
+        self.state = inst.get('State', {}).get('Name').lower()
+        self.tags_dict = dict([(d["Key"],d["Value"]) for d in tags_array])
+
+
+def IterInstances(instance_id=None):
+    params = {'region': None}
+    if instance_id:
+        params["instance-id"] = instance_id
+    j = AwsSystem("aws ec2 describe-instances", params)
+    if not j:
+        return # nothing to iterate
+    reservations = j.get("Reservations", [])
+    for reserve in reservations:
+        instances = reserve.get("Instances", [])
+        for inst in instances:
+            info = InstanceInfo(inst)
+            if info.state == 'terminated':
+                continue
+            yield info
+
+def FindInstance(instance_id):
+    for info in IterInstances(instance_id):
+        return info
+    return None
+
+
+def GetPublicIp(instance_id):
+    if not instance_id:
+        return None
+    for info in IterInstances(instance_id=instance_id):
+        return info.pub_ip
+    return None
+
+
+def ListInstances():
+    for inst in IterInstances():
+        print "instance {0}: ip: {1}  tags: {2} state: {3}".format(inst.id, inst.pub_ip, str(inst.tags_dict), inst.state)
+
+def DescribeInstance(instance_id):
+    cmd = "aws ec2 describe-instance-status"
+    params = {'instance-id': instance_id, 'region': None}
+    return AwsSystem(cmd, params, True)
+
+def SSHInstance(instance_id, pem_ppk_file):
+    if sys.platform == "win32":
+        SSHInstanceWin32(instance_id, pem_ppk_file)
+    else:
+        SSHInstanceUnix(instance_id, pem_ppk_file)
+
+def SSHInstanceWin32(instance_id, ppk_file):
+    # 'C:\Program Files (x86)\PuTTY\putty.exe'
+    # -ssh $HOST -l user -i private-key-file
+    putty = os.getenv("PUTTYEXE", 'C:\Program Files (x86)\PuTTY\putty.exe')
+    if not os.path.exists(putty):
+        print "ERROR - could not find putty exe : %s" % putty
+        print "Please define env variable PUTTYEXE"
+        return
+    instance = FindInstance(instance_id)
+    if not instance:
+        print "instance not found:", instance_id
+        return
+    ports = getDefault('ssh-tunnel-ports')
+    lflag = ""
+    if not ppk_file or not os.path.exists(ppk_file):
+        print "ERROR - no pem file found : ", ppk_file
+        exit(3)
+    for port in ports:
+        lflag += " -L %d:%s:%d" % (port, instance.pub_ip, port)
+    System("\"%s\" -ssh -l %s -i %s %s %s"% (putty, getDefault('ec2-username'), ppk_file, instance.pub_ip, lflag))
+
+
+# ssh to instance from laptop - this does NOT work on Windows
+def SSHInstanceUnix(instance_id, pem_file):
+    instance = FindInstance(instance_id)
+    if not instance:
+        print "instance not found:", instance_id
+        return
+    ports = getDefault('ssh-tunnel-ports')
+    lflag = ""
+    if not pem_file or not os.path.exists(pem_file):
+        print "ERROR - no pem file found : ", pem_file
+        exit(3)
+    for port in ports:
+        lflag += " -L %d:%s:%d" % (port, instance.pub_ip, port)
+    System("ssh -i %s %s@%s %s"% (pem_file, getDefault('ec2-username'), instance.pub_ip, lflag))
+
+# ssh to instance from laptop - this does NOT work on Windows
+def SCPInstances(instance_id, pem_file, src, dst):
+    instance = FindInstance(instance_id, pem_file)
+    if not instance:
+        print "instance not found:", instance_id
+        return
+    if not pem_file or not os.path.exists(pem_file):
+        print "ERROR - no pem file found : ", pem_file
+        exit(3)
+    System("scp -i %s %s@%s:%s" % (pem_file,src, getDefault('ec2-username'), instance.pub_ip, dst))
+
+
+
